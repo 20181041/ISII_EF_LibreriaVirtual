875cff880372e39ffe93198c4e300ebd3f895801
Andrea Lira <andrealf2001@gmail.com>
2021-11-11 05:24:02 -0500

    GuardarLibro

--- server/routes/libro.js
+++ server/routes/libro.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
+router.get('/GuardarLibro', async (req, res) => {
+    const libro = await pool.query('SELECT * from Libro');
+    for (var i = 0; i < libro.length; i++) {
+        var libros = [
+            { id: i.id, name: i.Nombre, precio: i.precio, autor: i.autor, tipo: i.Tipo_Transaccion, imperfectos: i.imperfectos, 
+                categoria: i.Categoria, estado: i.estado, usuario: i.ID_Usuario, url: i.url },
+        ];
+    }
+    var tagline = "No programming concept is complete without a cute animal mascot.";
 
+    res.render('GuardarLibro', {
+        libros: libros,
+        tagline: tagline
+    });
+});
 
 
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-//const { isLoggedIn } = require('../lib/auth');
-
+//const { isLoggedIn } = require('../lib/auth'); 
 //Escribimos las rutas que van a interactuar con la  tabla usuarios 
 
-router.get('/add' , (req, res) =>{
-
- res.render('ejemplo')
-
-
-
+router.get('/add', (req, res) => {
+    res.render('ejemplo')
 
 });
 
-
-
-router.get('/mostrar' , async (req, res) =>{
-
+router.get('/mostrar', async (req, res) => {
     const usuario = await pool.query('SELECT * FROM usuario');
-
-
-    console.log('el sistema ha recibido los datos',usuario);
-
+    console.log('el sistema ha recibido los datos', usuario);
     const legible = JSON.stringify(usuario);
-
-    console.log(legible, undefined , 2);
-    res.render('ejemplo')
-   
-   
-   });
+    console.log(legible, undefined, 2);
+    res.render('IngresarLibro')
+});
 
 
 module.exports = router;
