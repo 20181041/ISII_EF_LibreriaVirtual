31d656958c90ec7078e917cec25b8200a0c4345a
Andrea Lira <andrealf2001@gmail.com>
2021-11-11 18:22:01 -0500

    guardar libro

--- server/routes/libro.js
+++ server/routes/libro.js
@@ -4,20 +4,25 @@ const pool = require('../database');
 
 router.get('/GuardarLibro', async (req, res) => {
     const libro = await pool.query('SELECT * from Libro');
+    const readable = JSON.stringify(libro);
     var libros = [];
-    for (var i = 0; i < libro.length; i++) {
+    for (var i = 0; i < readable.length; i++) {
         libros = [
             {
-                id: i.id, name: i.Nombre, precio: i.precio, autor: i.autor, tipo: i.Tipo_Transaccion, imperfectos: i.imperfectos,
-                categoria: i.Categoria, estado: i.estado, usuario: i.ID_Usuario, url: i.url
+                id: i.ID_Libro, name: i.Nombre,
+                precio: i.precio, autor: i.autor,
+                tipo: i.Tipo_Transaccion, imperfectos: i.imperfectos,
+                categoria: i.Categoria, estado: i.estado,
+                usuario: i.ID_Usuario, url: i.url
             },
         ];
     }
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+    console.log('el sistema ha recibido los datos', libro);
+    console.log('el sistema ha recibido los datos en json', readable);
 
     res.render('GuardarLibro', {
-        libros: libros,
-        tagline: tagline
+        libro: libro,
+        // tagline: tagline
     });
 });
 module.exports = router;
