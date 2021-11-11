3fde20b6a4fdd6596fe41dcac9f2f24a6cfa863a
clau3355 <73697504+clau3355@users.noreply.github.com>
2021-11-11 00:56:03 -0500

    Cambios 11/11/01

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,24 @@ router.get('/', async (req, res) => {
     res.send('Aquí irá la página principal');
 });
 
+router.get('/catalogo', async (req, res) => {
+    res.render('catalogo')
+});
 
+router.get('/carrito', async (req, res) => {
+    res.render('carrito')
+});
+
+router.get('/MiPerfil-Resumen', async (req, res) => {
+    res.send('MiperfilResumen');
+});
+
+router.get('/MiPerfil-Transaccion', async (req, res) => {
+    res.send('MiPerfilTrans');
+});
+
+router.get('/MiPerfil-Editar', async (req, res) => {
+    res.send('Miperfilmtr');
+});
 
 module.exports = router;
