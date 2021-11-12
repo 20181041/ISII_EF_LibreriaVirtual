bbe1e000eb94d903295372a388acb07d81e66c13
Andrea Lira <andrealf2001@gmail.com>
2021-11-12 17:22:01 -0500

    ultimo push

--- server/routes/catalogo.js
+++ server/routes/catalogo.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
- 
-<<<<<<< Updated upstream
-function buscar_id(a, b){
-    var us1;
-    
-    for(let x = 0; x < a.length; x++){
-        if(a[x].ID_Usuario == b){
-            us1 = a[x];
-        }
-    }
-    return us1
-}
 
-router.get('/catalogo/', async (req, res) => {
-    
-    const libro = await pool.query('SELECT * from libro');
-    const listaUsuarios = await pool.query('SELECT * FROM usuario');
-
-
-    console.log('el sistema ha recibido los datos', libro);
-=======
 router.get('/catalogo', async (req, res) => {
-    const libro = await pool.query('SELECT * from libro'); 
->>>>>>> Stashed changes
- 
+    const libro = await pool.query('SELECT * from libro');
+
     res.render('catalogo', {
         libro: libro,
-        
+
     });
 });
 
