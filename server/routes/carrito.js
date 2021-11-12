8883c739345e201b7b92912e2291a8d4ab17f1ef
clau3355 <73697504+clau3355@users.noreply.github.com>
2021-11-12 17:02:18 -0500

    cambios clau final

--- server/routes/carrito.js
+++ server/routes/carrito.js
@@ -8,7 +8,12 @@ const libro5 = ["5 recetas de pan","10", "Autor5", "Intercambio", "pág5 sucia",
 const libro6 = ["mulan","5", "Autor6", "Venta", "pág5 sucia", "comedia", "Completado", "cenicienta.png", "Completado"  ];
 const libro7 = ["10 historias de amor","20", "Autor7", "Pendiente", "pág5 sucia", "comedia", "Pendiente", "cenicienta.png", "Comunicacion"  ];
 const libro8 = ["zombies","0", "Autor8", "Venta", "pág5 sucia", "comedia", "Pendiente", "cenicienta.png", "Transaccion"  ];
-var listalibros = [libro1, libro2, libro3, libro4, libro5, libro6, libro7, libro8];
+
+
+
+router.post('/carrito/:id/:idLibro', async(req, res) => {
+    
+});
 
 router.get('/carrito/:id', async (req, res) => {
      var listadelibros = listalibros;
--- server/routes/catalogo.js
+++ server/routes/catalogo.js
@@ -2,13 +2,28 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
  
-router.get('/catalogo', async (req, res) => {
+function buscar_id(a, b){
+    var us1;
+    
+    for(let x = 0; x < a.length; x++){
+        if(a[x].ID_Usuario == b){
+            us1 = a[x];
+        }
+    }
+    return us1
+}
+
+router.get('/catalogo/', async (req, res) => {
+    
     const libro = await pool.query('SELECT * from libro');
+    const listaUsuarios = await pool.query('SELECT * FROM usuario');
+
 
     console.log('el sistema ha recibido los datos', libro);
  
     res.render('catalogo', {
-        libro: libro
+        libro: libro,
+        
     });
 });
 
