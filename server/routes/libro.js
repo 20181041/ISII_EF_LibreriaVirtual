96dec31d648efa3e67ab7c156f7f43840cfb4cca
Andrea Lira <andrealf2001@gmail.com>
2021-11-12 14:52:42 -0500

    de todo un poco aaaaaaa

--- server/routes/libro.js
+++ server/routes/libro.js
@@ -6,13 +6,15 @@ router.get('/actualizar', async (req, res) => {
     const libro = await pool.query('SELECT * from Libro');
     const categorias = await pool.query('SELECT * from categorias');
     const estados = await pool.query('SELECT * from estados');
+    const tipo_transaccion = await pool.query('SELECT * from tipo_transaccion');
 
     console.log('el sistema ha recibido los datos', libro);
 
     res.render('GuardarLibro', {
         libro: libro,
         categorias: categorias,
-        estados: estados
+        estados: estados,
+        tipo_transaccion: tipo_transaccion,
     });
 });
 
@@ -31,5 +33,33 @@ router.get('/ingresar', async (req, res) => {
         tipo_transaccion: tipo_transaccion,
     });
 });
+router.post('/ingresoLibro/:id', async (req, res) => {
+    const libro = await pool.query('SELECT * from Libro');
+    const categorias = await pool.query('SELECT * from categorias');
+    const estados = await pool.query('SELECT * from estados');
+    const tipo_transaccion = await pool.query('SELECT * from tipo_transaccion');
 
+    const ID_Libro = libro.length + 1;
+    const Titulo = req.body.titulo;
+    const Precio = req.body.precio;
+    const Autor = req.body.autor;
+    const Tipo_transaccion = req.body.Tipo_Transaccion.value;
+    const Imperfectos = req.body.imperfectos;
+    const Categoria = req.body.categoria.value;
+    const Estado = req.body.estado.value;
+    const ID_Usuario = req.params.id;
+
+    console.log(Tipo_transaccion);
+
+    pool.query(`INSERT INTO libro VALUES('${ID_Libro}', '${Titulo}','${Precio}', ${Autor}', ${Tipo_transaccion}',${Imperfectos}', ${Categoria}', ${Estado}',${ID_Usuario}' );`)
+
+    console.log("yey");
+    res.redirect('IngresarLibro', {
+        libro: libro,
+        categorias: categorias,
+        estados: estados,
+        tipo_transaccion: tipo_transaccion,
+    });
+
+});
 module.exports = router;
--- server/routes/transaccion.js
+++ server/routes/transaccion.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-
-
 router.get('/ZonaComunicacion', async (req, res) => {
     const usuario = await pool.query('SELECT * FROM usuario');
     console.log('el sistema ha recibido los datos', usuario);
-    
     res.render('ZonaComunicacion', {
         usuario: usuario,
     });
@@ -16,22 +13,13 @@ router.get('/ZonaComunicacion', async (req, res) => {
 router.get('/ZonaConfirmacion', async (req, res) => {
     const usuario = await pool.query('SELECT * FROM usuario');
     console.log('el sistema ha recibido los datos', usuario);
-    
     res.render('ZonaConfirmacion', {
         usuario: usuario,
     });
-
-    
 });
-router.get('/ZonaTransaccion', async (req, res) => {
-    
 
+router.get('/ZonaTransaccion', async (req, res) => {
     res.render('ZonaTransaccion')
 });
-
-
-
-
-
 
 module.exports = router;
