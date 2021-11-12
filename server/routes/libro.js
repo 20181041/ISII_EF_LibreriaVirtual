b1208e26d12ee78082f8eacdc41e6c6eb501ff18
Andrea Lira <andrealf2001@gmail.com>
2021-11-11 20:14:25 -0500

    Actualiza libro

--- server/routes/libro.js
+++ server/routes/libro.js
@@ -5,24 +5,11 @@ const pool = require('../database');
 router.get('/GuardarLibro', async (req, res) => {
     const libro = await pool.query('SELECT * from Libro');
     const readable = JSON.stringify(libro);
-    var libros = [];
-    for (var i = 0; i < readable.length; i++) {
-        libros = [
-            {
-                id: i.ID_Libro, name: i.Nombre,
-                precio: i.precio, autor: i.autor,
-                tipo: i.Tipo_Transaccion, imperfectos: i.imperfectos,
-                categoria: i.Categoria, estado: i.estado,
-                usuario: i.ID_Usuario, url: i.url
-            },
-        ];
-    }
     console.log('el sistema ha recibido los datos', libro);
     console.log('el sistema ha recibido los datos en json', readable);
 
     res.render('GuardarLibro', {
         libro: libro,
-        // tagline: tagline
     });
 });
 module.exports = router;
