1845a52088f89aa0179bc9ece835dd7547d231a3
20180084 <20180084@aloe.ulima.edu.pe>
2021-11-11 22:32:33 -0500

    Zonas_Completos
    aa

--- server/routes/transaccion.js
+++ server/routes/transaccion.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
 
 
+router.get('/ZonaComunicacion', async (req, res) => {
+    const usuario = await pool.query('SELECT * FROM usuario');
+    console.log('el sistema ha recibido los datos', usuario);
+    
+    res.render('ZonaComunicacion', {
+        usuario: usuario,
+    });
+});
+
+router.get('/ZonaConfirmacion', async (req, res) => {
+    const usuario = await pool.query('SELECT * FROM usuario');
+    console.log('el sistema ha recibido los datos', usuario);
+    
+    res.render('ZonaConfirmacion', {
+        usuario: usuario,
+    });
+
+    
+});
+router.get('/ZonaTransaccion', async (req, res) => {
+    
+
+    res.render('ZonaTransaccion')
+});
 
 
 
