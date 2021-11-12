dfd9d3d85923c8b95a0b4f510f47f80eafeb22fc
dzegarra99 <73700391+dzegarra99@users.noreply.github.com>
2021-11-12 02:52:22 -0500

    cambios catalogo

--- server/routes/catalogo.js
+++ server/routes/catalogo.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
  
+router.get('/catalogo', async (req, res) => {
+    const libro = await pool.query('SELECT * from libro');
+
+    console.log('el sistema ha recibido los datos', libro);
+ 
+    res.render('catalogo', {
+        libro: libro
+    });
+});
 
 module.exports = router;
