a91ad30acd505184a82ba6d8694e6d7fd11f7314
Andrea Lira <andrealf2001@gmail.com>
2021-11-16 10:33:44 -0500

    actualizar y agregar libro

--- server/routes/libro.js
+++ server/routes/libro.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-router.get('/actualizar', async (req, res) => {
-    const libro = await pool.query(`SELECT * from Libro where ID_Libro =1`);
-    const categorias = await pool.query('SELECT * from categorias');
-    const estados = await pool.query('SELECT * from estados');
-    const tipo_transaccion = await pool.query('SELECT * from tipo_transaccion');
-    res.render('GuardarLibro', {
-        libro: libro,
-        categorias: categorias,
-        estados: estados,
-        tipo_transaccion: tipo_transaccion,
-    });
-});
+
 router.get('/ingresar', async (req, res) => {
     const libro = await pool.query('SELECT * from Libro');
     const categorias = await pool.query('SELECT * from categorias');
@@ -33,13 +22,26 @@ router.post('/ingresoLibro/:id', async (req, res) => {
     const Autor = req.body.autor;
     const tipoTrans = req.body.tipoTrans;
     const Imperfectos = req.body.imperfectos;
+    const descripcion = req.body.descripcion;
     const Categoria = req.body.categoria;
     const Estado = req.body.estado;
     const ID_Usuario = req.params.id;
     console.log('Este es el usuario', user.ID_Usuario);
-    pool.query(`INSERT INTO libro VALUES('${ID_Libro}', '${Titulo}','${Precio}', '${Autor}', '${tipoTrans}','${Imperfectos}', '${Categoria}', '${Estado}','${ID_Usuario}');`)
+    pool.query(`INSERT INTO libro VALUES('${ID_Libro}', '${Titulo}','${Precio}', '${Autor}', '${tipoTrans}','${Imperfectos}', '${Categoria}', '${Estado}','${ID_Usuario}','${descripcion}');`)
 
     res.redirect('/ingresar');
+});
+router.get('/actualizar/:idLibro', async (req, res) => {
+    const libro = await pool.query(`SELECT * from Libro where ID_Libro = ${req.params.idLibro}`);
+    const categorias = await pool.query('SELECT * from categorias');
+    const estados = await pool.query('SELECT * from estados');
+    const tipo_transaccion = await pool.query('SELECT * from tipo_transaccion');
+    res.render('GuardarLibro', {
+        libro: libro,
+        categorias: categorias,
+        estados: estados,
+        tipo_transaccion: tipo_transaccion,
+    });
 });
 router.post('/actualizoLibro/:id/:idLibro', async (req, res) => {
     const ID_Libro = req.params.idLibro;
@@ -48,11 +50,12 @@ router.post('/actualizoLibro/:id/:idLibro', async (req, res) => {
     const Autor = req.body.autor;
     const tipoTrans = req.body.tipoTrans;
     const Imperfectos = req.body.imperfectos;
+    const descripcion = req.body.descripcion;
     const Categoria = req.body.categoria;
     const Estado = req.body.estado;
     const ID_Usuario = req.params.id;
 
-    pool.query(`UPDATE usuario SET Nombre='${Titulo}', Precio='${Precio}', Autor='${Autor}', Tipo_Transaccion='${tipoTrans}',  Imperfectos='${Imperfectos}', Categoria='${Categoria}', Estado='${Estado}', ID_Usuario=${ID_Usuario} WHERE ID_Libro= ${ID_Libro}`)
+    pool.query(`UPDATE usuario SET Nombre='${Titulo}', Precio='${Precio}', Autor='${Autor}', Tipo_Transaccion='${tipoTrans}',  Imperfectos='${Imperfectos}', Categoria='${Categoria}', Estado='${Estado}', ID_Usuario='${ID_Usuario}', descripcion='${descripcion}' WHERE ID_Libro= '${ID_Libro}'`)
 
 
     res.redirect(`/actualizar/${ID_Libro}`);
