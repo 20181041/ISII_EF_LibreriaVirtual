72619e02cd1a3dab4ee14536ecf17262725fce41
Andrea Lira <andrealf2001@gmail.com>
2021-12-03 00:37:06 -0500

    arreglo catalogo

--- server/routes/catalogo.js
+++ server/routes/catalogo.js
@@ -2,19 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
-const { GetCarrito} = require('../arreglo')
-const {GetListaLibros} = require ('../Generales')
+const { GetCarrito } = require('../arreglo')
+const { GetListaLibros } = require('../Generales')
 
 
 
 
-router.get('/catalogo', isLoggedIn, async (req, res) => {
+router.get('/catalogo', isLoggedIn, async(req, res) => {
 
     const libro = await GetListaLibros();
-    console.log(`hola tu id es ${req.user.ID_Usuario} y tu nombres es ${req.user.Username} ` )
+    console.log(`hola tu id es ${req.user.ID_Usuario} y tu nombres es ${req.user.Username} `)
     carrito = await GetCarrito(req.user.ID_Usuario);
     res.render('catalogo', {
         libro: libro,
+        imagen: req.user.img,
         nombre: req.user.Username,
         id_usuario: req.user.ID_Usuario,
         LibrosEnCarrito: carrito.length
